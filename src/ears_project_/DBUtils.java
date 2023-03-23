92084903fb80970a7a8e1ca92e10badd54523728
amandeepsingh12 <amandeepsingh12@instance-1.c.clear-answer-380922.internal.>
2023-03-23 21:06:26 +0000

    Create Search all completed along with Backend

--- src/ears_project_/DBUtils.java
+++ src/ears_project_/DBUtils.java
@@ -25,223 +25,251 @@ import javafx.stage.Stage;
  * @author Aman
  */
 public class DBUtils {
-    public static void changeScene(ActionEvent event,String fxmlFile,String title, String username)
-    {
+
+    public static void changeScene(ActionEvent event, String fxmlFile, String title, String username) {
         Parent root = null;
-        
-        if(username!=null)
-        {
-            try{
+
+        if (username != null) {
+            try {
                 FXMLLoader loader = new FXMLLoader(DBUtils.class.getResource(fxmlFile));
-                root= loader.load();
+                root = loader.load();
                 HomeController homecontroller = loader.getController();
                 // there is function call for him only
                 homecontroller.storedata(username);
-            }
-            catch(IOException e)
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        else
-        {
-            try{
-            root=   FXMLLoader.load(DBUtils.class.getResource(fxmlFile));
-            }
-            catch(IOException e)
-            {
+        } else {
+            try {
+                root = FXMLLoader.load(DBUtils.class.getResource(fxmlFile));
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-            
+
         }
-        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setTitle(title);
-        stage.setScene(new Scene(root,800,600));
+        stage.setScene(new Scene(root, 800, 600));
         stage.show();
-        
+
     }
-    
-    public static boolean signUpUser(ActionEvent event, String username,String password,String contact,int designation_id,int department_id) throws ClassNotFoundException, SQLException
-    {
-        
+
+    public static boolean signUpUser(ActionEvent event, String username, String password, String contact, int designation_id, int department_id) throws ClassNotFoundException, SQLException {
+
         {
             Connection conn = new jdbcconnect().init();
-            String query="select count(*) as count1 from ears.users where username= '"+username+"'";
+            String query = "select count(*) as count1 from ears.users where username= '" + username + "'";
             Statement st1 = conn.createStatement();
-            ResultSet rs1 =st1.executeQuery(query);
-            boolean useralreadyexists=false;
-            if(rs1.next())
-            {
-                System.out.println("the value of count1 is "+rs1.getInt("count1"));
-                if(rs1.getInt("count1")>=1)
-                {
-                    useralreadyexists=true;
-                    System.out.println(" user already exists  "+useralreadyexists);
+            ResultSet rs1 = st1.executeQuery(query);
+            boolean useralreadyexists = false;
+            if (rs1.next()) {
+                System.out.println("the value of count1 is " + rs1.getInt("count1"));
+                if (rs1.getInt("count1") >= 1) {
+                    useralreadyexists = true;
+                    System.out.println(" user already exists  " + useralreadyexists);
                     return false;
-                    
+
                 }
-                
+
             }
             //insert data into user table
-            if(useralreadyexists==false)
-            {
-                query="insert into ears.users (username,password,contact,designation_id,department_id)  values('"+username+"','"+password+"','"+contact+"',"+designation_id+","+department_id+")";
+            if (useralreadyexists == false) {
+                query = "insert into ears.users (username,password,contact,designation_id,department_id)  values('" + username + "','" + password + "','" + contact + "'," + designation_id + "," + department_id + ")";
                 //st1 = conn.createStatement();
-                boolean p=st1.execute(query);
-                System.out.println("insert query == "+p);
-                
+                boolean p = st1.execute(query);
+                System.out.println("insert query == " + p);
+
             }
             conn.close();
         }
-       
+
         return true;
-    
+
     }
-    
+
     // log in function
-    
-    public static boolean logInUser(ActionEvent event,String username,String password) throws ClassNotFoundException, SQLException
-    {     
+    public static boolean logInUser(ActionEvent event, String username, String password) throws ClassNotFoundException, SQLException {
         Connection con = new jdbcconnect().init();
         boolean p;
-        
-            String query="select count(*) as count1 from ears.users where username= '"+username+"' and password= '"+password+"'";
-            Statement st1 = con.createStatement();
-            ResultSet rs1 =st1.executeQuery(query);
-            
-            if(rs1.next())
-            {
-                if(rs1.getInt("count1")>=1)
-                {
-                    con.close();
-                    return true;
-                }
-                else
-                {
-                    System.out.println("we can't find username and password for login validation");
-                    con.close();
-                    return false;
-                }
+
+        String query = "select count(*) as count1 from ears.users where username= '" + username + "' and password= '" + password + "'";
+        Statement st1 = con.createStatement();
+        ResultSet rs1 = st1.executeQuery(query);
+
+        if (rs1.next()) {
+            if (rs1.getInt("count1") >= 1) {
+                con.close();
+                return true;
+            } else {
+                System.out.println("we can't find username and password for login validation");
+                con.close();
+                return false;
             }
-            
-        
+        }
+
         return false;
     }
-    
+
     //applicant submission function
-    public static boolean Applicantuser(ActionEvent event, String username,String email,String contact,String description,int designation_id,int department_id) throws ClassNotFoundException, SQLException
-    {
-        
+    public static boolean Applicantuser(ActionEvent event, String username, String email, String contact, String description, int designation_id, int department_id) throws ClassNotFoundException, SQLException {
+
         {
             Connection conn = new jdbcconnect().init();
-            String query="select count(*) as count1 from ears.applicant where email= '"+email+"'";
+            String query = "select count(*) as count1 from ears.applicant where email= '" + email + "'";
             Statement st1 = conn.createStatement();
-            ResultSet rs1 =st1.executeQuery(query);
-            boolean useralreadyexists=false;
-            if(rs1.next())
-            {
-                System.out.println("the value of count1 is "+rs1.getInt("count1"));
-                if(rs1.getInt("count1")>=1)
-                {
-                    useralreadyexists=true;
-                    System.out.println(" user already exists  "+useralreadyexists);
+            ResultSet rs1 = st1.executeQuery(query);
+            boolean useralreadyexists = false;
+            if (rs1.next()) {
+                System.out.println("the value of count1 is " + rs1.getInt("count1"));
+                if (rs1.getInt("count1") >= 1) {
+                    useralreadyexists = true;
+                    System.out.println(" user already exists  " + useralreadyexists);
                     return false;
-                    
+
                 }
-                
+
             }
             //insert data into user table
-            if(useralreadyexists==false)
-            {
-                query="insert into ears.applicant (username,email,contact,designation_id,department_id,description)  values('"+username+"','"+email+"','"+contact+"',"+designation_id+","+department_id+",'"+description+"')";
+            if (useralreadyexists == false) {
+                query = "insert into ears.applicant (username,email,contact,designation_id,department_id,description)  values('" + username + "','" + email + "','" + contact + "'," + designation_id + "," + department_id + ",'" + description + "')";
                 //st1 = conn.createStatement();
-                boolean p=st1.execute(query);
-                System.out.println("insert query == "+p);
-                
+                boolean p = st1.execute(query);
+                System.out.println("insert query == " + p);
+
             }
             conn.close();
         }
-       
+
         return true;
-    
+
     }
-    
-    
-    
-    
+
     //check if user is HOD, Dean or not
-    public static boolean designation_check( String username) throws ClassNotFoundException, SQLException
-    {
+    public static boolean designation_check(String username) throws ClassNotFoundException, SQLException {
         //return true->if <=2 else false
         {
             Connection conn = new jdbcconnect().init();
-            String query="select designation_id as count1  from ears.users where username= '"+username+"'";
+            String query = "select designation_id as count1  from ears.users where username= '" + username + "'";
             Statement st1 = conn.createStatement();
-            ResultSet rs1 =st1.executeQuery(query);
-            
-            if(rs1.next())
-            {
-                System.out.println("the value of count1 is "+rs1.getInt("count1"));
-                if(rs1.getInt("count1")<=2)
-                {
+            ResultSet rs1 = st1.executeQuery(query);
+
+            if (rs1.next()) {
+                System.out.println("the value of count1 is " + rs1.getInt("count1"));
+                if (rs1.getInt("count1") <= 2) {
                     return true;
-                    
-                }
-                else
-                {
+
+                } else {
                     return false;
                 }
-                
+
             }
-            
+
             conn.close();
         }
-       
+
         return false;
-    
+
     }
-    
-    public static List<CreateSearchModel> SearchList(String username) throws ClassNotFoundException, SQLException
-    {
-        int department_id=1;
+
+    public static List<CreateSearchModel> SearchList(String username) throws ClassNotFoundException, SQLException {
+        int department_id = 1;
         Connection conn = new jdbcconnect().init();
-            String query="select department_id as count1  from ears.users where username= '"+username+"'";
-            Statement st1 = conn.createStatement();
-            ResultSet rs1 =st1.executeQuery(query);
-            
-            if(rs1.next())
-            {
-                System.out.println("the value of department_id is "+rs1.getInt("count1"));
-                 department_id =rs1.getInt("count1");
-                
-            }
-            
-            //getting data of all employees of same department with rank>=2;
-            query="select username,designation_name from ears.users  u left join ears.designation_table d on u.designation_id=d.designation_id where department_id="+department_id+" and u.designation_id>2;";
-            st1 = conn.createStatement();
-            rs1 =st1.executeQuery(query);
-            
-            List<CreateSearchModel> list = new ArrayList();
-            CreateSearchModel csm;
-            int count =1;
-            while(rs1.next())
-            {
-                System.out.println(rs1.getString(1));
-                System.out.println(rs1.getString(2));
-                
-                csm =new CreateSearchModel();
-                csm.setId(Integer.toString(count));
-                csm.setUsername(rs1.getString(1));
-                csm.setDesignation(rs1.getString(2));
-        
-                list.add(csm);
-                count++;
-                
-            }
-            
-        
+        String query = "select department_id as count1  from ears.users where username= '" + username + "'";
+        Statement st1 = conn.createStatement();
+        ResultSet rs1 = st1.executeQuery(query);
+
+        if (rs1.next()) {
+            System.out.println("the value of department_id is " + rs1.getInt("count1"));
+            department_id = rs1.getInt("count1");
+
+        }
+
+        //getting data of all employees of same department with rank>=2;
+        query = "select username,designation_name from ears.users  u left join ears.designation_table d on u.designation_id=d.designation_id where department_id=" + department_id + " and u.designation_id>2;";
+        st1 = conn.createStatement();
+        rs1 = st1.executeQuery(query);
+
+        List<CreateSearchModel> list = new ArrayList();
+        CreateSearchModel csm;
+        int count = 1;
+        while (rs1.next()) {
+            System.out.println(rs1.getString(1));
+            System.out.println(rs1.getString(2));
+
+            csm = new CreateSearchModel();
+            csm.setId(Integer.toString(count));
+            csm.setUsername(rs1.getString(1));
+            csm.setDesignation(rs1.getString(2));
+
+            list.add(csm);
+            count++;
+
+        }
+
+        conn.close();
         return list;
-        
+
     }
-    
+
+    public static boolean addsearchlistdata(String username, String committee_name, int designation_id, String chairperson, ArrayList<String> search_list) throws ClassNotFoundException, SQLException {
+        //to find department id of current user
+        Connection conn = new jdbcconnect().init();
+        String query = "select department_id as count1  from ears.users where username= '" + username + "'";
+        Statement st1 = conn.createStatement();
+        ResultSet rs1 = st1.executeQuery(query);
+        
+        int department_id=1;
+
+        if (rs1.next()) {
+            System.out.println("the value of department_id is " + rs1.getInt("count1"));
+            department_id = rs1.getInt("count1");
+
+        }
+        
+        //check if committee with same name exists or not
+        query = "select count(committee_id) as count1  from ears.committee_table where title = '" + committee_name + "'";
+        //st1 = conn.createStatement();
+        rs1 = st1.executeQuery(query);
+        
+        if (rs1.next()) {
+            System.out.println("the value of department_id is " + rs1.getInt("count1"));
+            if(rs1.getInt("count1")>=1)
+            {
+                return false;
+            }
+            
+
+        }
+        
+        
+        
+        
+        //add data to committee_table
+        query = "insert into ears.committee_table (title,department_id,chairperson_name,designation_id)  values('" + committee_name + "'," + department_id + ",'" + chairperson + "'," + designation_id + ")";
+        boolean p = st1.execute(query);
+        System.out.println("insert query of committee_table == " + p);
+        
+        //get the committee_id from same table to insert data into intermediate table
+        query = "select committee_id as count1  from ears.committee_table where title= '" + committee_name + "'";
+        //st1 = conn.createStatement();
+        rs1 = st1.executeQuery(query);
+        int committee_id=1;
+        if (rs1.next()) {
+            System.out.println("the value of department_id is " + rs1.getInt("count1"));
+            committee_id= rs1.getInt("count1");
+        }
+        
+        //insert committee_members data into committee_member_transition_table
+        for(int i=0;i<search_list.size();i++)
+        {
+            query = "insert into ears.committee_member_transition_table (committee_id,username)  values(" + committee_id + ",'" + search_list.get(i) + "')";
+            p = st1.execute(query);
+            System.out.println("insert query of committee_member_transition_table == " + p);
+            
+            
+        }
+        
+        return true;
+    }
+
 }
--- src/ears_project_/HomeController.java
+++ src/ears_project_/HomeController.java
@@ -21,6 +21,7 @@ import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -37,149 +38,173 @@ import javafx.scene.layout.HBox;
  * @author Aman
  */
 public class HomeController implements Initializable {
+
     private String username;
     private ArrayList<String> search_list;
 
     @FXML
-    private Button submit_first_btn,cancel_first_btn,submit_four_btn,logout_four_btn;
-    
+    private Button submit_first_btn, cancel_first_btn, submit_four_btn, logout_four_btn;
+
     @FXML
-    private TextField committee_first_tf,username_four_tf,password_four_tf,contact_four_tf;
-    
-    private int first_designation=3;
-    private String first_chairperson="";
-    @FXML 
-    private ComboBox designation_first_cb,chairperson_first_cb,department_four_cb,designation_four_cb;
-    
+    private TextField committee_first_tf, username_four_tf, password_four_tf, contact_four_tf;
+
+    private int first_designation = 3;
+    private String first_chairperson = "";
+    @FXML
+    private ComboBox designation_first_cb, chairperson_first_cb, department_four_cb, designation_four_cb;
+
     @FXML
     private TabPane tabpane_tp;
-    
+
     @FXML
-    private Tab tab1,tab2,tab3,tab4;
-    
+    private Tab tab1, tab2, tab3, tab4;
+
     @FXML
     private Label access_lb;
-    
+
     @FXML
-    void tabSelected_1()
-    {
+    void tabSelected_1() {
         System.out.println(1);
         System.out.println(this.username);
     }
-    
-    
+
     @FXML
-    void tabSelected_2()
-    {
+    void tabSelected_2() {
         System.out.println(2);
     }
-    
+
     @FXML
-    void tabSelected_3()
-    {
+    void tabSelected_3() {
         System.out.println(3);
     }
-    
+
     @FXML
-    void tabSelected_4()
-    {
+    void tabSelected_4() {
         System.out.println(4);
     }
+
     //for chairpeerson CB selection
     @FXML
     void select_chairperson_first(ActionEvent event) {
         System.out.println(chairperson_first_cb.getSelectionModel().getSelectedItem());
-        first_chairperson =chairperson_first_cb.getSelectionModel().getSelectedItem().toString();
+        first_chairperson = chairperson_first_cb.getSelectionModel().getSelectedItem().toString();
 
     }
+
     //for designation CB of applicant
     @FXML
     void select_designation_first(ActionEvent event) {
         System.out.println(designation_first_cb.getSelectionModel().getSelectedIndex());
-        first_designation =designation_first_cb.getSelectionModel().getSelectedIndex()+3;
+        first_designation = designation_first_cb.getSelectionModel().getSelectedIndex() + 3;
     }
 
-    
-    
     @FXML
     private VBox committe_member_vbox;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         search_list = new ArrayList<String>();
+        search_list.clear();
         System.out.println(this.username);
         //combo box chairperson populating data 
-        ObservableList<String> cp_list =FXCollections.observableArrayList();
-        
-         //combo box designation populating data 
-        ObservableList<String> dst_list =FXCollections.observableArrayList("Senior Lecturer","Associate Professor","PhD Scholar");
+        ObservableList<String> cp_list = FXCollections.observableArrayList();
+
+        //combo box designation populating data 
+        ObservableList<String> dst_list = FXCollections.observableArrayList("Senior Lecturer", "Associate Professor", "PhD Scholar");
         designation_first_cb.setItems(dst_list);
-        
+
         // populating search data into the scrollpane's vBox
-        List<CreateSearchModel> csm= new ArrayList<>();
-        
+        List<CreateSearchModel> csm = new ArrayList<>();
+
         try {
-            System.out.println("this.username is "+this.username);
+            System.out.println("this.username is " + this.username);
             csm = new ArrayList<>(DBUtils.SearchList(this.username));
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
             Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        for(int i=0;i<csm.size();i++)
-        {
+        for (int i = 0; i < csm.size(); i++) {
             cp_list.add(i, csm.get(i).getUsername());
-            System.out.println(csm.get(i).getId()+ "   "+csm.get(i).getUsername()+"   "+csm.get(i).getDesignation());
+            System.out.println(csm.get(i).getId() + "   " + csm.get(i).getUsername() + "   " + csm.get(i).getDesignation());
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setLocation(getClass().getResource("createsearch_item.fxml"));
-            String usrname =csm.get(i).getUsername();
-            try{
-                HBox hbox =fxmlLoader.load();
-                CreateSearchController csc =fxmlLoader.getController();
+            String usrname = csm.get(i).getUsername();
+            try {
+                HBox hbox = fxmlLoader.load();
+                CreateSearchController csc = fxmlLoader.getController();
                 csc.setData(csm.get(i));
-                csc.addCodeHandler(MouseEvent.MOUSE_CLICKED, e->{
-                    System.out.println("clciked");
+                csc.addCodeHandler(MouseEvent.MOUSE_CLICKED, e -> {
+                    System.out.println("clicked");
                     search_list.add(usrname);
-                    
+
                 });
-                committe_member_vbox.getChildren().add(hbox);        
-        }   catch (IOException ex) {
+                committe_member_vbox.getChildren().add(hbox);
+            } catch (IOException ex) {
                 Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
             }
-        
-    }
+
+        }
         //adding list to chairperson CB
         chairperson_first_cb.setItems(cp_list);
-        
 
         //Submitting all the data of committee to DB
-        submit_first_btn.setOnAction(new EventHandler<ActionEvent>(){
+        submit_first_btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 String committee_name = committee_first_tf.getText().trim();
                 int designation_id = first_designation;
                 String chairperson = first_chairperson;
-                for(int i=0;i<search_list.size();i++)
-                {
+                for (int i = 0; i < search_list.size(); i++) {
                     System.out.println(search_list.get(i));
                 }
-                
+                search_list.add(chairperson);
+                if (committee_name.isEmpty() == true || search_list.size() == 1) {
+                    //please add the info
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("Please add all the information");
+                    alert.show();
+                } else {
+
+               
+                    try {
+                        boolean p = DBUtils.addsearchlistdata(username, committee_name, designation_id, chairperson, search_list);
+                        if (p == false) {
+                            //Committee already exists
+                            Alert alert = new Alert(Alert.AlertType.ERROR);
+                            alert.setContentText("Error: Committee already exists");
+                            alert.show();
+
+                        }
+                        else
+                        {
+                            //Confirmation
+                            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                            alert.setContentText("Committee with name :"+committee_name+" has been created, having "+search_list.size()+" members");
+                            alert.show();
+                            
+                        }
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                committee_first_tf.clear();
+                search_list.clear();
+
             }
         });
     }
-    
-    
-    
-    public void storedata(String username)
-    {
-        this.username=username;
-        
+
+    public void storedata(String username) {
+        this.username = username;
+
         try {
-            System.out.println("username is "+ this.username);
-            boolean p =DBUtils.designation_check(this.username);
-            
-            if(p==false)
-            {
+            System.out.println("username is " + this.username);
+            boolean p = DBUtils.designation_check(this.username);
+
+            if (p == false) {
                 access_lb.setVisible(true);
                 submit_first_btn.setDisable(true);
                 cancel_first_btn.setDisable(true);
@@ -190,64 +215,59 @@ public class HomeController implements Initializable {
             Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    private List<CreateSearchModel> SearchList()
-    {
+
+    private List<CreateSearchModel> SearchList() {
         List<CreateSearchModel> list = new ArrayList();
-        CreateSearchModel csm =new CreateSearchModel();
+        CreateSearchModel csm = new CreateSearchModel();
         csm.setId("1");
         csm.setUsername("Aman");
         csm.setDesignation("Head of department");
-        
+
         list.add(csm);
-        
-        csm =new CreateSearchModel();
+
+        csm = new CreateSearchModel();
         csm.setId("2");
         csm.setUsername("Prof1");
         csm.setDesignation("Head of department");
-        
+
         list.add(csm);
-        
-        csm =new CreateSearchModel();
+
+        csm = new CreateSearchModel();
         csm.setId("3");
         csm.setUsername("Prof3");
         csm.setDesignation("Head of department");
-        
+
         list.add(csm);
-        
-        
-        csm =new CreateSearchModel();
+
+        csm = new CreateSearchModel();
         csm.setId("4");
         csm.setUsername("Prof4");
         csm.setDesignation("Head of department");
-        
+
         list.add(csm);
-        
-        
-        csm =new CreateSearchModel();
+
+        csm = new CreateSearchModel();
         csm.setId("5");
         csm.setUsername("Prof5");
         csm.setDesignation("Head of department");
-        
+
         list.add(csm);
-        
-        csm =new CreateSearchModel();
+
+        csm = new CreateSearchModel();
         csm.setId("6");
         csm.setUsername("Prof6");
         csm.setDesignation("Head of department");
-        
+
         list.add(csm);
-        
-        csm =new CreateSearchModel();
+
+        csm = new CreateSearchModel();
         csm.setId("7");
         csm.setUsername("Prof7");
         csm.setDesignation("Head of department");
-        
+
         list.add(csm);
-        
+
         return list;
     }
-    
-    
-    
+
 }
